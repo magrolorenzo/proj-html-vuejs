d769d76e48f89f5185a033258ea9c868ed1b9032
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-30 17:00:51 +0100

    Sezione jumbotron quasi completata
    Da aggiungere sfondo verde ad h1, migliorare effetto sfumato su sfondo

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,26 @@ var app = new Vue({
     el: "#root",
 
     data:{
+
         phone_number: "+1 (305) 1234-5678",
-        e_mail: "hello@example.com"
+        e_mail: "hello@example.com",
+        navbar_items: [
+            "Home",
+            "About",
+            "Projects",
+            "Process",
+            "Testimonials"
+        ],
+
+        current_navbar_index: 0
+
+    },
+
+    methods:{
+        navbar_select(i){
+            console.log(i); 
+            return this.current_navbar_index = i;
+        }
     }
 
 });
